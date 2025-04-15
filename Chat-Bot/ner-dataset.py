6296b85d017e3c87d7de27870a1dd1e4d56a2a68
vanhuong <tovanhuong008@gmail.com>
2025-04-15 16:00:16 +0700

    initialize Graduation Thesis project and fix Chat bot problems

--- Chat-Bot/ner-dataset.py
+++ Chat-Bot/ner-dataset.py
@@ -966,5 +966,106 @@
   ("Ray nau la benh gi o lua.",
   {
     "entities": [(0, 8, "thong-tin-sau-benh-lua")],
-  }),
+  }),  ("phân bón NPK Phú Mỹ 16-8-17 + TE có không?", {
+        "entities": [(10, 34, "FERTILIZER")],
+    }),
+    ("chi tiết về phân NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(15, 39, "FERTILIZER")],
+    }),
+    ("tôi muốn hỏi về NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(18, 42, "FERTILIZER")],
+    }),
+    ("tôi muốn biết thêm thông tin về NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(33, 57, "FERTILIZER")],
+    }),
+    ("chỉ số của NPK Phú Mỹ 16-8-17 + TE.", {
+        "entities": [(11, 35, "FERTILIZER")],
+    }),
+    ("phân bón NPK Phú Mỹ 16-8-17 + TE có không?", {
+        "entities": [(10, 34, "FERTILIZER")],
+    }),
+    ("chi tiết về phân NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(15, 39, "FERTILIZER")],
+    }),
+    ("tôi muốn hỏi về NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(18, 42, "FERTILIZER")],
+    }),
+    ("tôi muốn biết thêm thông tin về NPK Phú Mỹ 16-8-17 + TE", {
+        "entities": [(33, 57, "FERTILIZER")],
+    }),
+    ("chỉ số của NPK Phú Mỹ 16-8-17 + TE.", {
+        "entities": [(11, 35, "FERTILIZER")],
+    }),
+    ("Tôi cần thuốc trị sâu cuốn lá", {
+        "entities": [(18, 30, "PEST")],
+    }),
+    ("Tôi cần thuốc trị (sâu cuốn lá)", {
+        "entities": [(18, 30, "PEST")],
+    }),
+    ("Cho tôi hỏi bệnh vàng lá ở lúa là gì?", {
+        "entities": [(18, 26, "thong-tin-sau-benh-lua")],
+    }),
+    ("Cho tôi hỏi bệnh vàng lá ở lúa là gì.", {
+        "entities": [(18, 26, "thong-tin-sau-benh-lua")],
+    }),
+    ("Phân hữu cơ là gì?", {
+        "entities": [(0, 12, "FERTILIZER_TYPE")],
+    }),
+    ("phân hữu cơ.", {
+        "entities": [(0, 12, "FERTILIZER_TYPE")],
+    }),
+    ("khái niệm phân hữu cơ", {
+        "entities": [(10, 22, "FERTILIZER_TYPE")],
+    }),
+    ("như thế nào gọi là phân hữu cơ?", {
+        "entities": [(21, 33, "FERTILIZER_TYPE")],
+    }),
+    ("Phân hữ cơ là gì?", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("phân hưu cơ.", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("khái niệm phan hữu cơ", {
+        "entities": [(10, 22, "FERTILIZER_TYPE")],
+    }),
+    ("như thế nào gọi là phan hữ cơ?", {
+        "entities": [(21, 33, "FERTILIZER_TYPE")],
+    }),
+    ("Phân vô cơ là gì?", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("phân vô cơ.", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("khái niệm phân vô cơ", {
+        "entities": [(10, 21, "FERTILIZER_TYPE")],
+    }),
+    ("như thế nào gọi là phân vô cơ?", {
+        "entities": [(21, 32, "FERTILIZER_TYPE")],
+    }),
+    ("Phân vố cơ là gì?", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("phân vơ cơ.", {
+        "entities": [(0, 11, "FERTILIZER_TYPE")],
+    }),
+    ("khái niệm phan vô cơ", {
+        "entities": [(10, 21, "FERTILIZER_TYPE")],
+    }),
+    ("như thế nào gọi là phan vố cơ?", {
+        "entities": [(21, 32, "FERTILIZER_TYPE")],
+    }),
+    ("Phân bón lá là gì?", {
+        "entities": [(0, 12, "FERTILIZER_TYPE")],
+    }),
+    ("phân bón lá.", {
+        "entities": [(0, 12, "FERTILIZER_TYPE")],
+    }),
+    ("khái niệm phân bón lá", {
+        "entities": [(10, 22, "FERTILIZER_TYPE")],
+    }),
+    ("như thế nào gọi là phân bón lá?", {
+        "entities": [(21, 33, "FERTILIZER_TYPE")],
+    }),
 ]
